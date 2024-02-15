b62e2e6aba9a82e967807f1355989ff5cbe9cfa9
Aryam Gupta <87194432+Aryamgupta@users.noreply.github.com>
2024-02-15 19:44:29 +0530

    responsive navbarAdded

--- index.js
+++ index.js
@@ -10,14 +10,27 @@ const subCat = document.querySelector(".subcat");
 const subCatDiv = document.querySelector(".subcat>div");
 const mainNavigation = document.querySelector(".v2navigaton");
 const mainC = document.querySelector(".mainC");
+const mobileNavCloseBtn = document.querySelectorAll(".mobileNavCloseBtn");
+const mobileChildMainConatiner = document.querySelector(
+  ".mobileChildCategoiresConatiner"
+);
+const mobileNavigationPanel = document.querySelector(".mobileNavigationPanel");
+const mobileMainListConatier = document.querySelector(".mobileMainCategory");
 
-// hoverEffectBtn.onmouseover = () => {
-//   subCat.style.display = "block";
-// };
+const mobileNavigationOpen = document.querySelector(
+  "#mobileViewNavigationOpen"
+);
 
-// hoverEffectBtn.onmouseout = () => {
+mobileNavigationOpen.addEventListener("click", () => {
+  mobileNavigationPanel.style.display = "flex";
+});
 
-// };
+mobileNavCloseBtn.forEach((element) => {
+  element.addEventListener("click", () => {
+    mobileNavigationPanel.style.display = "none";
+    mobileChildMainConatiner.style.display = "none";
+  });
+});
 
 const setMainCategories = () => {
   mainCategories.forEach((element) => {
@@ -38,6 +51,96 @@ const setMainCategories = () => {
 
     v2navbarItemConatiner.appendChild(node);
   });
+};
+
+const setMobileMainCategory = () => {
+  mainCategories.forEach((element) => {
+    let node = document.createElement("li");
+    let insideNode1 = document.createElement("img");
+    insideNode1.src = `${element.images[0] && element.images[0].imageUrl}`;
+    insideNode1.width = "95";
+    insideNode1.height = "95";
+    let pNode = document.createElement("p");
+    pNode.innerHTML = `${element.categoryName}`;
+    let insideNode2 = document.createElement("img");
+    insideNode2.src = "./icons/goFor.svg";
+    insideNode2.width = "32";
+    insideNode2.height = "32";
+    insideNode2.style.border = "none";
+    insideNode2.style.marginTop = "-20";
+    insideNode2.setAttribute("data-urlKey", `${element.urlKey}`);
+    insideNode2.onclick = (e) => {
+      setMobileSubCategory(insideNode2.getAttribute("data-urlKey"));
+    };
+    node.appendChild(insideNode1);
+    node.appendChild(pNode);
+    node.appendChild(insideNode2);
+    // console.log(insideNode2);
+    mobileMainListConatier.appendChild(node);
+  });
+};
+
+const setMobileSubCategory = (key) => {
+  const subCategoryList = subCategorycompleteData[key].subCategory;
+  const childeCategoryList = subCategorycompleteData[key].childCategory;
+  let u1 = document.createElement("div");
+  u1.classList.add("mainCategoryHeadingholde");
+  let u1BtnNode1 = document.createElement("button");
+  u1BtnNode1.innerHTML = `<img src="./icons/prevBtn.svg" />`;
+  u1BtnNode1.onclick = () => {
+    mobileChildMainConatiner.innerHTML = "";
+    mobileChildMainConatiner.style.display = "none";
+  };
+  u1.appendChild(u1BtnNode1);
+  let u1p = document.createElement("p");
+  u1p.innerHTML = `${key}`;
+  let u1btn2 = document.createElement("button");
+  u1btn2.innerHTML = `&#x2715;`;
+  u1btn2.classList.add("closeBtn");
+  u1btn2.classList.add("mobileNavCloseBtn");
+  u1btn2.onclick = () => {
+    mobileNavigationPanel.style.display = "none";
+    mobileChildMainConatiner.style.display = "none";
+  };
+  u1.appendChild(u1p);
+  u1.appendChild(u1btn2);
+  console.log(subCategoryList);
+  //   adding real lists
+
+  let fullChilds = document.createElement("div");
+  fullChilds.classList.add("mobilechildCategorylist");
+
+  subCategoryList.forEach((el) => {
+    let subCcat = document.createElement("div");
+    subCcat.classList.add("mobilechildCategory");
+    subCcat.innerHTML = `<div class="mobilechildCategoryHeading">
+            ${el.categoryName}
+            <div class="movilechildheadline"></div>
+          </div>`;
+    // console.log(el);
+    let ulSubCat = document.createElement("ul");
+    ulSubCat.classList.add("mobileActualChildCategoryList");
+
+    let filteredData = childeCategoryList.filter((l) => l.parentId === el.id);
+    console.log(filteredData);
+    filteredData.forEach((k) => {
+      let childNode = document.createElement("li");
+      childNode.classList.add("mobileChildcategorylistItem");
+      childNode.innerHTML = `<img src="" width="95px" height="95px" />
+              <p>${k.categoryName}</p>`;
+      ulSubCat.appendChild(childNode);
+      console.log(k.images);
+    });
+
+    subCcat.appendChild(ulSubCat);
+    fullChilds.appendChild(subCcat);
+  });
+
+  //   console.log(fullChilds);
+  mobileChildMainConatiner.appendChild(u1);
+  mobileChildMainConatiner.appendChild(fullChilds);
+  console.log(mobileChildMainConatiner);
+  mobileChildMainConatiner.style.display = "block";
 };
 
 const setSubCategory = (key) => {
@@ -62,7 +165,6 @@ const setSubCategory = (key) => {
       node.appendChild(childNode);
     });
   });
-  console.log(subCategoryList);
 };
 
 const setChildCategory = (id, childCategoryList) => {
@@ -126,7 +228,7 @@ const fetchMenuItems = async () => {
       mainCategories.forEach((element) => {
         categoriesWebId.push(element.urlKey);
       });
-      setMainCategories();
+      window.innerWidth > 786 ? setMainCategories() : setMobileMainCategory();
       subcatergoriesdetailsFetch();
     }
   };
